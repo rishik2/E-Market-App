e7d2117956cb8d9ad2a94eaa61174b54dccd2979
Rishi Kohli <rishikohli@Rishis-MacBook-Air.local>
2021-11-25 17:46:04 +0530

    Updates

---
+++ precache-manifest.ef571cd8ebc769431f3e91f83a154859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce631fe10ba451ad30c1",
+    "url": "/E-Market-App.git/static/css/main.b20d3db2.chunk.css"
+  },
+  {
+    "revision": "ce631fe10ba451ad30c1",
+    "url": "/E-Market-App.git/static/js/main.ce631fe1.chunk.js"
+  },
+  {
+    "revision": "36391a3eb9a4190cbe8c",
+    "url": "/E-Market-App.git/static/css/1.fdf2b724.chunk.css"
+  },
+  {
+    "revision": "36391a3eb9a4190cbe8c",
+    "url": "/E-Market-App.git/static/js/1.36391a3e.chunk.js"
+  },
+  {
+    "revision": "81f72ab31e2d71a86d1b",
+    "url": "/E-Market-App.git/static/js/runtime~main.81f72ab3.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-Market-App.git/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "7af7b5c326a468af41e900219a458bcf",
+    "url": "/E-Market-App.git/index.html"
+  }
+];
